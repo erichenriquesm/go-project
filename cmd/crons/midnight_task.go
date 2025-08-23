46cc3109594d828c692ff473a812e4191162e18d
Eric Motta <erichenriquedeveloper@gmail.com>
2025-08-23 18:39:26 -0300

    ok

---
+++ cmd/crons/midnight_task.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    fmt.Println("Tarefa da meia-noite iniciada!")
+
+    // Exemplo: chamar alguma função do seu pacote user
+    // user.SomeFunction()
+
+    // Apenas para demonstrar a execução
+    fmt.Println("Hora atual:", time.Now())
+}
