626e77ffbc0eb8e06e2660a95790c3895db1f782
Eric Motta <erichenriquedeveloper@gmail.com>
2025-08-23 18:55:39 -0300

    ok

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
 	server.GET("/ping", func(c *gin.Context) {
+		url := "https://webhook.site/ab01db3b-b40f-4ea4-a98c-33ab41ceaeb7"
+
+		jsonData := []byte(`{"message": "ping"}`)
+		response, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		defer response.Body.Close()
+
+		body, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(body))
+
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
